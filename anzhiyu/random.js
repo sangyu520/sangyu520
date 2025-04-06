5c90e14a00e1f2e45e9f24447c31c062e0c82563
sangyu520 <2035027466@qq.com>
2025-04-06 08:56:28 +0800

    Site updated: 2025-04-06 08:56:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/03/hello-world/","2025/04/05/markown/","2025/04/05/如何写一篇文章/"];function toRandomPost(){
+var posts=["2025/04/05/markown/","2025/04/03/hello-world/","2025/04/05/如何写一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
