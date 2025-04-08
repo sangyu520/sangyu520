0bef9d9a52fc3e0db19871ea226cb2422c29e5de
sangyu520 <2035027466@qq.com>
2025-04-08 09:13:47 +0800

    Site updated: 2025-04-08 09:13:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/03/hello-world/","2025/04/05/markown/","2025/04/05/如何写一篇文章/","2025/04/07/react实现/","2025/04/07/如何在hexo里面实现类似动态播放视频/","2025/04/07/我搭建hexo博客之路/"];function toRandomPost(){
+var posts=["2025/04/05/markown/","2025/04/07/react实现/","2025/04/03/hello-world/","2025/04/05/如何写一篇文章/","2025/04/07/如何在hexo里面实现类似动态播放视频/","2025/04/07/我搭建hexo博客之路/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
