76ed5bf8c3cd589b4d9736c824830f7fdef3c0d7
sangyu520 <2035027466@qq.com>
2025-05-20 09:34:13 +0800

    Site updated: 2025-05-20 09:33:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/14/hello-world/","2025/05/19/CET6-words/"];function toRandomPost(){
+var posts=["2025/05/19/CET6-words/","2025/04/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
